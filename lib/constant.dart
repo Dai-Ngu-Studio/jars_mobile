430e2997528047ba7c19702de7ad49de5ab6f2d5
Nguyễn Dũng <dpron12345@gmail.com>
2022-02-11 19:26:47 +0700

    Create constant.dart

---
+++ lib/constant.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF3f81ee);
+const kSecondaryColor = Color(0xFF7B8490);
+const kAnimationDuration = Duration(milliseconds: 200);
+
+const MaterialColor jarsColor = MaterialColor(
+  0xFF3F81EE,
+  <int, Color>{
+    50: Color(0xFFdae8ff),
+    100: Color(0xFFc1d9ff),
+    200: Color(0xFF97bbf6),
+    300: Color(0xFF7eacfa),
+    400: Color(0xFF5e98f8),
+    500: Color(0xFF3f81ee),
+    600: Color(0xFF3c7ae0),
+    700: Color(0xFF3771d0),
+    800: Color(0xFF3569c0),
+    900: Color(0xFF3060af),
+  },
+);
