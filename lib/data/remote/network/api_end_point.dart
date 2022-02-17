6ec79184d4a9bb4e2d64ced0ebde8a4ee5655303
Nguyễn Dũng <dpron12345@gmail.com>
2022-02-17 21:51:33 +0700

    rename api endpoint

--- lib/data/remote/network/api_end_point.dart
+++ lib/data/remote/network/api_end_point.dart
@@ -1,10 +1,11 @@
 class ApiEndPoint {
-  final String getAccount = "account";
-  final String getBill = "bill";
-  final String getBillDetail = "bill-detail";
-  final String getCategory = "category";
-  final String getCategoryWallet = "category-wallet";
-  final String getNote = "note";
-  final String getTransaction = "transaction";
-  final String getWallet = "wallet";
+  final String getAccount = "Accounts";
+  final String getBill = "Bills";
+  final String getBillDetail = "BillDetails";
+  final String getCategory = "Categories";
+  final String getCategoryWallet = "CategoryWallets";
+  final String getContract = "Contracts";
+  final String getNote = "Notes";
+  final String getTransaction = "Transactions";
+  final String getWallet = "Wallets";
 }
