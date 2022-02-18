0895ee05f919607d3d6bd8af3ed3f815b4c894d8
Nguyễn Dũng <dpron12345@gmail.com>
2022-02-18 13:27:19 +0700

    fix wrong place fcm

--- lib/views/screens/home/home_screen.dart
+++ lib/views/screens/home/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:jars_mobile/data/models/tab_icon_data.dart';
+import 'package:jars_mobile/service/local_notification/local_notification_service.dart';
 import 'package:jars_mobile/views/screens/home/bottom_bar_view.dart';
 import 'package:jars_mobile/views/screens/home/components/body.dart';
 
@@ -35,7 +37,32 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
       vsync: this,
     );
     tabBody = const HomeScreen();
-    super.initState();
+
+    LocalNotificationService.initialize(context);
+
+    // give message on which user taps and it opened the app from terminated state (app close)
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      if (message != null) {
+        final routeFromMessage = message.data["route"];
+        Navigator.of(context).pushNamed(routeFromMessage);
+      }
+    });
+
+    // just in foreground (app must open)
+    FirebaseMessaging.onMessage.listen((message) {
+      if (message.notification != null) {
+        print(message.notification!.title);
+        print(message.notification!.body);
+      }
+
+      LocalNotificationService.display(message);
+    });
+
+    // this just work when app in background (app not open) and user taps on the notification
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      final routeFromMessage = message.data["route"];
+      Navigator.of(context).pushNamed(routeFromMessage);
+    });
   }
 
   @override
--- lib/views/screens/login/init_widget.dart
+++ lib/views/screens/login/init_widget.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:jars_mobile/views/screens/home/home_screen.dart';
 import 'package:jars_mobile/views/screens/login/login_screen.dart';
-import 'package:jars_mobile/service/local_notification/local_notification_service.dart';
 
 class InitializerWidget extends StatefulWidget {
   const InitializerWidget({Key? key}) : super(key: key);
@@ -21,32 +19,6 @@ class _InitializerWidgetState extends State<InitializerWidget> {
     super.initState();
     _auth = FirebaseAuth.instance;
     _user = _auth.currentUser;
-
-    LocalNotificationService.initialize(context);
-
-    // give message on which user taps and it opened the app from terminated state (app close)
-    FirebaseMessaging.instance.getInitialMessage().then((message) {
-      if (message != null) {
-        final routeFromMessage = message.data["route"];
-        Navigator.of(context).pushNamed(routeFromMessage);
-      }
-    });
-
-    // just in foreground (app must open)
-    FirebaseMessaging.onMessage.listen((message) {
-      if (message.notification != null) {
-        print(message.notification!.title);
-        print(message.notification!.body);
-      }
-
-      LocalNotificationService.display(message);
-    });
-
-    // this just work when app in background (app not open) and user taps on the notification
-    FirebaseMessaging.onMessageOpenedApp.listen((message) {
-      final routeFromMessage = message.data["route"];
-      Navigator.of(context).pushNamed(routeFromMessage);
-    });
   }
 
   @override
