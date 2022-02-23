0f5d0dbe5e7ab340acd67f3245c76ace392a59f0
Nguyễn Dũng <dpron12345@gmail.com>
2022-02-23 15:52:56 +0700

    Create account_view_model.dart

---
+++ lib/view_model/account_view_model.dart
@@ -0,0 +1,26 @@
+import 'package:jars_mobile/data/models/account.dart';
+import 'package:jars_mobile/data/remote/response/api_response.dart';
+import 'package:jars_mobile/data/repository/account_repository_impl.dart';
+
+class AccountViewModel {
+  final _accountRepo = AccountRepositoryImpl();
+
+  ApiResponse<Account> account = ApiResponse.loading();
+
+  void _setAccount(ApiResponse<Account> response) {
+    print(response);
+    account = response;
+  }
+
+  Future<void> login({required String idToken, String? fcmToken}) async {
+    _setAccount(ApiResponse.loading());
+    _accountRepo
+        .login(idToken: idToken, fcmToken: fcmToken)
+        .then((_) => _setAccount(ApiResponse.completed(null)))
+        .onError(
+          (error, stackTrace) => _setAccount(
+            ApiResponse.error(error.toString()),
+          ),
+        );
+  }
+}
