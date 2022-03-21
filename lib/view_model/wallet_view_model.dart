73d3a0de98892b0965083a132771d04b958950a0
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-22 00:12:33 +0700

    add putWallet, generateSixJars (wallet_view_model)

--- lib/view_model/wallet_view_model.dart
+++ lib/view_model/wallet_view_model.dart
@@ -1,22 +1,48 @@
+import 'package:flutter/material.dart';
 import 'package:jars_mobile/data/models/wallet.dart';
 import 'package:jars_mobile/data/remote/response/api_response.dart';
 import 'package:jars_mobile/data/repository/wallet_repository_impl.dart';
 
-class WalletViewModel {
+class WalletViewModel extends ChangeNotifier {
   final _walletRepo = WalletRepositoryImpl();
 
-  ApiResponse<List<Wallet>> wallet = ApiResponse.loading();
+  ApiResponse<List> wallet = ApiResponse.loading();
 
-  void _setWallet(ApiResponse<List<Wallet>> response) {
+  void _setWallet(ApiResponse<List> response) {
     print(response);
     wallet = response;
+    notifyListeners();
   }
 
-  Future<void> getWallet({required String idToken}) async {
+  Future getWallet({required String idToken}) async {
     _setWallet(ApiResponse.loading());
     await _walletRepo
         .getWallets(idToken: idToken)
         .then((value) => _setWallet(ApiResponse.completed(value)))
+        .onError(
+          (error, stackTrace) => _setWallet(
+            ApiResponse.error(error.toString()),
+          ),
+        );
+  }
+
+  Future putWallet({required String idToken, required Wallet wallet}) async {
+    _setWallet(ApiResponse.loading());
+    await _walletRepo
+        .updateWallet(idToken: idToken, wallet: wallet)
+        .whenComplete(() => _setWallet(ApiResponse.completed(null)))
+        .onError(
+          (error, stackTrace) => _setWallet(
+            ApiResponse.error(error.toString()),
+          ),
+        );
+  }
+
+  Future<void> generateSixJars({required String idToken}) async {
+    _setWallet(ApiResponse.loading());
+    _walletRepo
+        .generateSixJars(idToken: idToken)
+        .whenComplete(() => _setWallet(ApiResponse.completed(null)))
         .onError(
           (error, stackTrace) => _setWallet(
             ApiResponse.error(error.toString()),
