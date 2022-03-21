66cd7e5b6d2d05e5dda83cd6f73707b58c3544c9
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-21 21:56:10 +0700

    Add generateSixJars (wallet_repository)

--- lib/data/repository/wallet_repository.dart
+++ lib/data/repository/wallet_repository.dart
@@ -1,9 +1,11 @@
 import 'package:jars_mobile/data/models/wallet.dart';
 
 abstract class WalletRepository {
-  Future<List<Wallet>> getWallets({required String idToken});
+  Future getWallets({required String idToken});
 
   Future<Wallet> getWallet({required String idToken, required String walletId});
 
   Future<void> updateWallet({required String idToken, required Wallet wallet});
+
+  Future<void> generateSixJars({required String idToken});
 }
