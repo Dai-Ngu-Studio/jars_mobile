dbc68721e5ed3a072c4a2845ac9fbfafa480e077
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-21 21:58:14 +0700

    implement getWallets, updateWallet, generateSixJars (wallet_repository_impl)

--- lib/data/repository/wallet_repository_impl.dart
+++ lib/data/repository/wallet_repository_impl.dart
@@ -1,11 +1,35 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:jars_mobile/data/models/wallet.dart';
+import 'package:jars_mobile/data/remote/network/api_end_point.dart';
+import 'package:jars_mobile/data/remote/network/base_api_service.dart';
+import 'package:jars_mobile/data/remote/network/network_api_service.dart';
 import 'package:jars_mobile/data/repository/wallet_repository.dart';
 
 class WalletRepositoryImpl extends WalletRepository {
+  final BaseApiService _apiService = NetworkApiService();
+
   @override
-  Future<List<Wallet>> getWallets({required String idToken}) {
-    // TODO: implement getWallets
-    throw UnimplementedError();
+  Future getWallets({required String idToken}) async {
+    try {
+      dynamic response = await _apiService.getResponse(
+        ApiEndPoint().wallet,
+        header: Map<String, String>.from({
+          "Authorization": "Bearer $idToken",
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        }),
+      );
+      final json = [];
+
+      for (var item in response) {
+        json.add(Wallet.fromJson(item));
+      }
+      return json;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 
   @override
@@ -21,8 +45,35 @@ class WalletRepositoryImpl extends WalletRepository {
   Future<void> updateWallet({
     required String idToken,
     required Wallet wallet,
-  }) {
-    // TODO: implement updateWallet
-    throw UnimplementedError();
+  }) async {
+    try {
+      print(jsonEncode(wallet.toJson()));
+      dynamic response = await _apiService.putResponse(
+        "${ApiEndPoint().wallet}/${wallet.id}",
+        header: Map<String, String>.from({
+          "Authorization": "Bearer $idToken",
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        }),
+        body: jsonEncode(wallet.toJson()),
+      );
+      log('WalletRepositoryImpl :: updateWallet :: response: $response');
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  Future generateSixJars({required String idToken}) async {
+    try {
+      dynamic response = await _apiService.postResponse(
+        ApiEndPoint().wallet,
+        function: "six-wallets",
+        header: Map<String, String>.from({"Authorization": "Bearer $idToken"}),
+      );
+      log('WalletRepositoryImpl :: generateSixJars :: response: $response');
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
