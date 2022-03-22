206b07a93563645b1e34d96ae49be56ae7873dc3
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-22 17:30:49 +0700

    add function parameter for getResponse and getResponseByID

--- lib/data/remote/network/base_api_service.dart
+++ lib/data/remote/network/base_api_service.dart
@@ -1,9 +1,14 @@
 abstract class BaseApiService {
   final String baseUrl = "http://jars-api.software/api/v1/";
 
-  Future<dynamic> getResponse(String url, {required header});
+  Future<dynamic> getResponse(String url, {String? function, required header});
 
-  Future<dynamic> getResponseByID(String url, {required header, dynamic id});
+  Future<dynamic> getResponseByID(
+    String url, {
+    String? function,
+    required header,
+    dynamic id,
+  });
 
   Future<dynamic> postResponse(
     String url, {
