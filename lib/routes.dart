56ba80f0fc09178893b47f217b86202e38484c71
ngianguyen81 <ngianguyen81@gmail.com>
2022-03-23 14:42:50 +0700

    add transaction details route & view

--- lib/routes.dart
+++ lib/routes.dart
@@ -5,6 +5,7 @@ import 'package:jars_mobile/views/screens/app_info/app_info_screen.dart';
 import 'package:jars_mobile/views/screens/factory_reset/factory_reset_screen.dart';
 import 'package:jars_mobile/views/screens/general_settings/general_settings_screen.dart';
 import 'package:jars_mobile/views/screens/login/login_screen.dart';
+import 'package:jars_mobile/views/screens/transaction_details/transaction_details_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => const LoginScreen(),
@@ -13,4 +14,5 @@ final Map<String, WidgetBuilder> routes = {
   AppInfoScreen.routeName: (context) => const AppInfoScreen(),
   GeneralSettingsScreen.routeName: (context) => const GeneralSettingsScreen(),
   FactoryResetScreen.routeName: (context) => const FactoryResetScreen(),
+  TransactionDetails.routeName: (context) => const TransactionDetails(),
 };
