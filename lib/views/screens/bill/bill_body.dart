3e4c64b5e78af6a49d9ee829d7eaf1038a9b5b43
ngianguyen81 <ngianguyen81@gmail.com>
2022-03-24 12:03:32 +0700

    bill list functional

--- lib/views/screens/bill/bill_body.dart
+++ lib/views/screens/bill/bill_body.dart
@@ -1,5 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:jars_mobile/constant.dart';
+import 'package:jars_mobile/data/models/bill.dart';
+import 'package:jars_mobile/data/remote/response/status.dart';
+import 'package:jars_mobile/view_model/bill_view_model.dart';
 import 'package:jars_mobile/views/screens/bill/components/bill_box.dart';
+import 'package:jars_mobile/views/widgets/loading.dart';
+import 'package:provider/provider.dart';
 
 class BillBody extends StatefulWidget {
   const BillBody({Key? key}) : super(key: key);
@@ -9,55 +16,96 @@ class BillBody extends StatefulWidget {
 }
 
 class _BillBodyState extends State<BillBody> {
-  List listData = [];
+  final _billVM = BillViewModel();
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  List<Bill> bills = [];
+
   @override
   void initState() {
-    listData = [
-      {
-        "id": 1,
-        "name": "tiền nhà",
-        "date": "2020-01-01",
-        "leftAmount": 100,
-        "amount": 200,
-      },
-      {
-        "id": 1,
-        "name": "tiền học",
-        "date": "2020-01-01",
-        "leftAmount": 100,
-        "amount": 200,
-      },
-      {
-        "id": 1,
-        "name": "tiền wifi",
-        "date": "2020-01-01",
-        "leftAmount": 100,
-        "amount": 200,
-      },
-      {
-        "id": 1,
-        "name": "tiền tập gym",
-        "date": "2020-01-01",
-        "leftAmount": 100,
-        "amount": 200,
-      },
-    ];
     super.initState();
+    _firebaseAuth.currentUser!.getIdToken().then((idToken) {
+      _billVM.getBills(idToken: idToken).whenComplete(() {
+        if (_billVM.bills.data == null) {
+          return;
+        }
+        for (var bill in _billVM.bills.data!) {
+          var _bill = Bill(
+            id: (bill as Bill).id,
+            name: bill.name,
+            date: bill.date,
+            leftAmount: bill.leftAmount,
+            amount: bill.amount,
+          );
+          bills.add(_bill);
+        }
+        bills = bills.reversed.toList();
+      });
+    });
+  }
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 50));
+    return true;
+  }
+
+  Widget getBillsViewUI() {
+    return FutureBuilder(
+      future: getData(),
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        if (!snapshot.hasData) {
+          return const SizedBox.shrink();
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text("Something went wrong! Please try again later."),
+          );
+        } else {
+          return ChangeNotifierProvider<BillViewModel>(
+            create: (BuildContext context) => _billVM,
+            child: Consumer<BillViewModel>(
+              builder: (context, viewModel, _) {
+                switch (viewModel.bills.status) {
+                  case Status.LOADING:
+                    return LoadingWidget();
+                  case Status.ERROR:
+                    return ErrorWidget(viewModel.bills.message ??
+                        "Something went wrong! Please try again later.");
+                  case Status.COMPLETED:
+                    return ListView.builder(
+                      itemCount: bills.length,
+                      itemBuilder: (context, index) {
+                        return BillBox(
+                            billId: bills[index].id!,
+                            name: bills[index].name!,
+                            date: bills[index].date!,
+                            leftAmount: bills[index].leftAmount!,
+                            amount: bills[index].amount!);
+                      },
+                    );
+                  default:
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+          );
+        }
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: listData.length,
-      itemBuilder: (context, index) {
-        return BillBox(
-          billId: listData[index]['id'],
-          name: listData[index]['name'],
-          date: listData[index]['date'],
-          leftAmount: listData[index]['leftAmount'],
-          amount: listData[index]['amount'],
-        );
-      },
-    );
+    return SafeArea(
+        child: Container(
+      color: kBackgroundColor,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            getBillsViewUI(),
+          ],
+        ),
+      ),
+    ));
   }
 }
